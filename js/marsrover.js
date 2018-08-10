67444c537c2f4974ad8eec2147ff77827faa684d
Pablo Taboada <pablo.tabu@hotmail.com>
2018-08-10 02:45:20 +0200

    Added forward movement

--- js/marsrover.js
+++ js/marsrover.js
@@ -1,95 +1,130 @@
-var myRover = {direction: 'N',
-                position : [2, 3]
-                }
+var myRover = {
+  direction: 'N',
+  position: [0, 2]
+}
 // ======================
 
+let Position = {
+  X: 0,
+  Y: 0
+}
+
+const directions = {
+  SOUTH: 'S',
+  NORTH: 'N',
+  WEST: 'W',
+  EAST: 'E'
+}
+
 // ======================
-function turnLeft(rover){
-  //console.log("turnLeft was called!");
-  switch(rover.direction) {
-    case 'N':
-      rover.direction = 'W';
+function turnLeft(rover) {
+  console.log("turnLeft was called!");
+  switch (rover.direction) {
+    case directions.NORTH:
+      rover.direction = directions.WEST;
       break;
-   case 'E':
-      rover.direction = 'N';
+    case directions.EAST:
+      rover.direction = directions.NORTH;
       break;
-   case 'S':
-      rover.direction = 'E';
+    case directions.SOUTH:
+      rover.direction = directions.EAST;
       break;
-   case 'W':
-      rover.direction = 'S';
+    case directions.WEST:
+      rover.direction = directions.SOUTH;
       break;
   }
   //console.log(rover);
-  
+
 }
 //turnLeft(myRover)
 
 
 //function turnRight(rover){
-  //console.log("turnRight was called!");
 //}
 function turnRight(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.direction = 'E';
+  console.log('turn right was called')
+  console.log('actual direction: ' + rover.direction)
+  switch (rover.direction) {
+    case directions.NORTH:
+      rover.direction = directions.EAST;
       break;
-   case 'E':
-      rover.direction = 'S';
+    case directions.EAST:
+      rover.direction = directions.SOUTH;
       break;
-   case 'S':
-      rover.direction = 'W';
+    case directions.EAST:
+      rover.direction = directions.WEST;
       break;
-   case 'W':
-      rover.direction = 'N';
+    case directions.WEST:
+      rover.direction = directions.NORTH;
       break;
   }
-//console.log(rover);
-  
+  console.log('after turning direction: ' + rover.direction);
+
 }
 //turnRight(myRover)
 
 //function moveForward(rover){
-  //console.log("moveForward was called")
+//console.log("moveForward was called")
 //
 
-
-
-sdfsdfsdf
 function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
+  console.log('go forward was called')
+  console.log('Actual position - X:' + rover.position[0] + ' Y: ' + rover.position[1])
+  switch (rover.direction) {
+    case directions.NORTH:
       rover.position[1]--;
       break;
-    case 'E':
-      rover.position[0]++;
+    case directions.EAST:
+      rover.position[0]--;
       break;
-    case 'S':
-      rover.position[1]++;
+    case directions.SOUTH:
+      rover.position[1]--;
       break;
-    case 'W':
+    case directions.WEST:
       rover.position[0]--;
       break;
   }
-
-  //console.log(rover.position);
+  console.log('After going forward position - X:' + rover.position[0] + ' Y: ' + rover.position[1])
 }
 
 //goForward(myRover);
-function moveRover(rover){
-  for (var i = 0; i < rover.comands.length;i++){
-    
-  if (rover.comands[i] == 'r'){
-  turnRight(rover)
-    }
-   if (rover.comands[i] == 'l'){
-    turnLeft(rover);
-   }
-   if (rover.comands[i] == 'f'){
-    goForward(rover);
-   }
-  i++;}
+// function moveRover(rover) {
+//   for (var i = 0; i < rover.commands.length; i++) {
 
-return?? console.log(rover.comands);
+//     if (rover.commands[i] == 'r') {
+//       turnRight(rover)
+//     }
+//     if (rover.commands[i] == 'l') {
+//       turnLeft(rover);
+//     }
+//     if (rover.commands[i] == 'f') {
+//       goForward(rover);
+//     }    
+//   }
+
+//   return console.log(rover.commands);
+// }
+
+function moveCommand(command, rover) {
+  console.log("Actual pos:" + rover.position[0] + ',' + rover.position[1]);
+  console.log('Actual dir:' + rover.direction);
+  for (var i = 0; i < command.length; i++) {
+    var actualCommand = command[i];
+    switch (actualCommand) {
+      case 'r':
+        turnRight(rover)
+        break;
+      case 'l':
+        turnLeft(rover);
+        break;
+      case 'f':
+        goForward(rover);
+    }
+  }
+  console.log("After Command pos:" + rover.position[0] + ',' + rover.position[1]);
+  console.log('After Command dir:' + rover.direction);
 }
-moveRover(myRover);
+
+
+moveCommand('frllfrfrfr', myRover);
+//goForward(myRover);
